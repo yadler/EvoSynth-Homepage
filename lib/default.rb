e97bd323a9b6da5cc6ab419ebd8ee8e842a6350d
Yves Adler <yves.adler@googlemail.com>
2010-10-15 17:56:26 +0200

    better source highlighting

--- lib/default.rb
+++ lib/default.rb
@@ -1,2 +1,13 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+
+include Nanoc3::Helpers::Filtering
+
+class SrcFilter < Nanoc3::Filter
+	identifier :src_filter
+
+	def run(content, params={})
+		content.gsub!('"', '\"')
+		'<div class="codebox">' + `echo "#{content}" | source-highlight --src-lang ruby --out-format html` + '</div>'
+	end
+end
